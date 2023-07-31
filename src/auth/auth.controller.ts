1b635f47f7e6129d4dac4ad5b2b8f19b82815278
김광현 <kwanghyeon.kim@mirero.co.kr>
2023-07-31 15:35:29 +0900

    [ADD] Swagger 적용

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { AuthGuard } from './auth.guard';
 import { LoginUserDto } from '../users/dto/users.loginuser.dto';
 import { CreateUserDto } from '../users/dto/users.createuser.dto'
 import { UpdateUserDto } from 'src/users/dto/users.updateuser.dto';
+import { ApiTags, ApiOperation, ApiCreatedResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -18,6 +19,8 @@ export class AuthController {
     }
 
     @Post('signup')
+    @ApiOperation({ summary: '사용자 생성 API', description: '사용자를 생성한다.' })
+    @ApiCreatedResponse({ description: '사용자를 생성한다.', type: User })
     async createUser(@Body() user:CreateUserDto): Promise<User> {
         return await this.authService.create(user)
     }
--- src/users/dto/users.createuser.dto.ts
+++ src/users/dto/users.createuser.dto.ts
@@ -1,12 +1,15 @@
 import { IsString } from "class-validator";
-
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
   @IsString()
+  @ApiProperty({ description: '아이디' })
   user_id: string;
 
+  @ApiProperty({ description: '이름' })
   @IsString()
   name: string;
 
+  @ApiProperty({ description: '비밀번호' })
   @IsString()
   password: string;
 }
