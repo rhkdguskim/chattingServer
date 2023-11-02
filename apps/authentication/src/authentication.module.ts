467510fd3500283e56724eadd25578ba13a49dbf
김광현 <rhkdguskim@naver.com>
2023-11-02 14:56:53 +0900

    Gitignore Fixed

--- apps/authentication/src/authentication.module.ts
+++ apps/authentication/src/authentication.module.ts
@@ -5,18 +5,9 @@ import { DatabaseModule } from "@app/common/module";
 import { User } from "@app/common/entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersService } from "./users.service";
-import { ClientsModule, Transport } from "@nestjs/microservices";
-import { AUTHORIZATION_SERVICE } from "@app/common/message/authorization";
 
 @Module({
   imports: [
-    ClientsModule.register([
-      {
-        name: AUTHORIZATION_SERVICE,
-        transport: Transport.TCP,
-        options: { host: "127.0.0.1", port: 3002 },
-      },
-    ]),
     DatabaseModule(),
     TypeOrmModule.forFeature([User]),
   ],
